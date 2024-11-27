e7f1d5ee5a84796464f548e4bc702439f605affc
Oisin McCrossan <oisinmccrossan@gmail.com>
2024-11-27 12:56:58 +0000

    Refactor App component to include Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
