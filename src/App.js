15340d841d559a54788acd93ba6ea55225b9d3b3
Oisin McCrossan <oisinmccrossan@gmail.com>
2024-11-27 13:01:18 +0000

    Refactor App component to include NavigationBar component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div>
+      <NavigationBar />
       <Header />
       <Content />
       <Footer />
