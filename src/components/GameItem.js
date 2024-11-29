2f2d7611e4542cb859128b25d3fba9f7e9b303ab
Oisin McCrossan <oisinmccrossan@gmail.com>
2024-11-29 14:58:56 +0000

    Refactor App component to include React Router DOM dependency

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
 
-const GameItem = ({ game }) => {
+const GameItem = (props) => {
+  useEffect(() => {
+    console.log("Game Item:", props.game);
+  }, [props.game]); // Only run this effect when the game prop changes
+
   return (
-    <li key={game.id}>
-      <h2>{game.Title} ({game.Year})</h2>
-      <img src={game.Poster} alt={game.Title} style={{ width: '200px' }} />
-    </li>
+    <div>
+      <Card>
+        <Card.Header>{props.game.Title}</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <img src={props.game.Poster} alt={props.game.Title} style={{ width: '200px' }} />
+            <footer>{props.game.Year}</footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
