cb6ace2d147d43f59dddbe3d5795015c77a53c36
Oisin McCrossan <oisinmccrossan@gmail.com>
2024-12-08 17:29:38 +0000

    Refactor App component to include React Router DOM and NavigationBar components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Content from './components/Content';
-import Footer from './components/Footer';
-import NavigationBar from './components/NavigationBar';
 import Read from './components/Read';
+import Create from './components/Create';
 
 function App() {
   return (
-    <div>
+    <Router>
       <NavigationBar />
-      <Header />
-      <Content />
-      <Read />
+      <Routes>
+        <Route path="/" element={<Content />} />
+        <Route path="/read" element={<Read />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
